6070258fb97453398aea86f0d2a54cf1ed5231a9
SamiChafik <sami.chafik00@gmail.com>
2025-05-15 12:34:18 +0100

    added routes to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TransactionComponent},
+    { path: 'form', component: TransactionComponent},
+    { path: 'transactionList', component: TransactionListComponent},
+    { path: '**', redirectTo: '' }
+];
