ad6988e504e5a066e56fdf1f5954ddd5de193af1
SamiChafik <sami.chafik00@gmail.com>
2025-05-18 20:33:10 +0100

    path to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { TransactionComponent } from './components/transaction/transaction.component';
-import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
+import { CategoryComponent } from './components/category/category.component';
+// import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
 export const routes: Routes = [
     { path: '', component: TransactionComponent},
-    { path: 'form', component: TransactionComponent},
-    { path: 'transactionList', component: TransactionListComponent},
+    { path: 'transactions', component: TransactionComponent},
+    // { path: 'transactionList', component: TransactionListComponent},
+    // { path: 'budget', component: BudgetComponent },
+    { path: 'categories', component: CategoryComponent },
     { path: '**', redirectTo: '' }
 ];
