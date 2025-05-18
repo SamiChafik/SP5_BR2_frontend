05590165608ceabc3e0f0220cd292d6c989ab345
SamiChafik <sami.chafik00@gmail.com>
2025-05-18 23:58:33 +0100

    home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { CategoryComponent } from './components/category/category.component';
 import { BudgetComponent } from './components/budget/budget.component';
+import { HomeComponent } from './components/home/home.component';
 // import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
 export const routes: Routes = [
     { path: '', component: TransactionComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'transactions', component: TransactionComponent},
     // { path: 'transactionList', component: TransactionListComponent},
     { path: 'budget', component: BudgetComponent },
