d2a5edad892831658c6f06f187c2164a4b4be629
SamiChafik <sami.chafik00@gmail.com>
2025-05-18 23:18:31 +0100

    added path to budget component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { CategoryComponent } from './components/category/category.component';
+import { BudgetComponent } from './components/budget/budget.component';
 // import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
 export const routes: Routes = [
     { path: '', component: TransactionComponent},
     { path: 'transactions', component: TransactionComponent},
     // { path: 'transactionList', component: TransactionListComponent},
-    // { path: 'budget', component: BudgetComponent },
+    { path: 'budget', component: BudgetComponent },
     { path: 'categories', component: CategoryComponent },
     { path: '**', redirectTo: '' }
 ];
